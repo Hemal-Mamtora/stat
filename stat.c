2d44f70e41051c5307f271120fd7b69fb61f3c63
Hemal-Mamtora <hemalmamtora1@gmail.com>
2022-04-13 20:59:44 -0500

    perms and hard links

--- stat.c
+++ stat.c
@@ -41,5 +41,24 @@ int main( int argc, char *argv[] )  {
     default:
       printf("The file type is unkowon\n");
   }
+  printf("\n");
+  printf("Does owner have read, write execute permission? %d\n", (info.st_mode & S_IRWXU));
+  printf("Does owner have read permission? %d\n", (info.st_mode & S_IRUSR));
+  printf("Does owner have write permission? %d\n", (info.st_mode & S_IWUSR));
+  printf("Does owner have execute permission? %d\n", (info.st_mode & S_IXUSR));
+
+  printf("Does group have read, write execute permission? %d\n", (info.st_mode & S_IRWXG));
+  printf("Does group have read permission? %d\n", (info.st_mode & S_IRGRP));
+  printf("Does group have write permission? %d\n", (info.st_mode & S_IWGRP));
+  printf("Does group have execute permission? %d\n", (info.st_mode & S_IXGRP));
+  
+  
+  printf("Do others have read, write execute permission? %d\n", (info.st_mode & S_IRWXO));
+  printf("Do others have read permission? %d\n", (info.st_mode & S_IROTH));
+  printf("Do others have write permission? %d\n", (info.st_mode & S_IWOTH));
+  printf("Do others have execute permission? %d\n", (info.st_mode & S_IXOTH));
+  printf("\n");
+  printf("4. st_link: %d. Number of hard links to the file\n", info.st_nlink);
+
   return 0;
 }
